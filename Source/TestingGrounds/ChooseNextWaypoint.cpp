8f5b40d8b76374e811934115840019d22ab3254f
HP-470G2-LCT\Kirill.Semenov <cyril.semenov@gmail.com>
2018-04-15 18:29:16 +0300

    TGERRORSTATE Crash after reparenting TP_ThirdPersonCharacter to the new C++ class

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -7,6 +7,7 @@ EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& Own
 {
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	
 	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
 	return EBTNodeResult::Succeeded;
 }
